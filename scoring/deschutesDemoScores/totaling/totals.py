9feea20929cb50ba9bad42d685ca587b154d7822
Brian Owen <briano.bend@icloud.com>
2019-07-29 14:01:55 -0700

    Added totaling options that include weight and reps

--- scoring/deschutesDemoScores/totaling/totals.py
+++ scoring/deschutesDemoScores/totaling/totals.py
@@ -1,29 +1,76 @@
 from django.db import models
-from deschutesDemoScores.models import Score
+from deschutesDemoScores.models import Score, Workout
 
 def getSingleWorkoutTotal(workout):
 	#TODO - update to work for single workouts when fully loaded
-	setOfScores = Score.objects.filter(workout = 6)
-	setOfScores = sorted(setOfScores, key=lambda x: (x.minutes, x.seconds, -int(x.reps or 0)))
-	listOfScores = []
-	rank = 1
-	for score in setOfScores:
-		recordedScore = orderedScore(score, rank)
-		listOfScores.append(recordedScore)
-		rank += 1
-	i = 0
-	#tie breaks - should clean up
-	for score in listOfScores:
-		print(listOfScores[i])
-		isTie = False
-		if i > 1:
-			if listOfScores[i].score.minutes == listOfScores[i-1].score.minutes:
-				if listOfScores[i].score.seconds == listOfScores[i-1].score.seconds:
-					if listOfScores[i].score.reps == listOfScores[i-1].score.reps:
-						isTie = True
-		if isTie:
-			listOfScores[i].rank = listOfScores[i-1].rank
-		i += 1
+	workoutProperties = Workout.objects.get(id = 6)
+	print(workoutProperties)
+	print(workoutProperties.scoringStyle)
+	setOfScores = Score.objects.filter(workout = workoutProperties.id)
+	print(setOfScores[0].reps)
+	#TODO - eliminate some repetition in the code if possible/reasonable
+	if workoutProperties.scoringStyle == 'T':
+		setOfScores = sorted(setOfScores, key=lambda x: (x.minutes, x.seconds, -int(x.reps or 0)))
+		listOfScores = []
+		rank = 1
+		for score in setOfScores:
+			recordedScore = orderedScore(score, rank)
+			listOfScores.append(recordedScore)
+			rank += 1
+		i = 0
+		#tie breaks - should clean up
+		for score in listOfScores:
+			print(listOfScores[i])
+			isTie = False
+			if i > 1:
+				if listOfScores[i].score.minutes == listOfScores[i-1].score.minutes:
+					if listOfScores[i].score.seconds == listOfScores[i-1].score.seconds:
+						if listOfScores[i].score.reps == listOfScores[i-1].score.reps:
+							isTie = True
+			if isTie:
+				listOfScores[i].rank = listOfScores[i-1].rank
+			i += 1
+
+	elif workoutProperties.scoringStyle == 'R':
+		setOfScores = sorted(setOfScores, key=lambda x: (-int(x.reps or 0)))
+		listOfScores = []
+		rank = 1
+		for score in setOfScores:
+			recordedScore = orderedScore(score, rank)
+			listOfScores.append(recordedScore)
+			rank += 1
+		i = 0
+		for score in listOfScores:
+			print(listOfScores[i])
+			isTie = False
+			if i > 1:
+				if listOfScores[i].score.reps == listOfScores[i-1].score.reps:
+					isTie = True
+			if isTie:
+				listOfScores[i].rank = listOfScores[i-1].rank
+			i += 1
+
+	elif workoutProperties.scoringStyle == 'W':
+		setOfScores = sorted(setOfScores, key=lambda x: (-int(x.weight or 0)))
+		listOfScores = []
+		rank = 1
+		for score in setOfScores:
+			recordedScore = orderedScore(score, rank)
+			listOfScores.append(recordedScore)
+			rank += 1
+		i = 0
+		for score in listOfScores:
+			print(listOfScores[i])
+			isTie = False
+			if i > 1:
+				if listOfScores[i].score.weight == listOfScores[i-1].score.weight:
+					isTie = True
+			if isTie:
+				listOfScores[i].rank = listOfScores[i-1].rank
+			i += 1
+
+	else: 
+		listOfScores = []
 
 	return listOfScores
 
