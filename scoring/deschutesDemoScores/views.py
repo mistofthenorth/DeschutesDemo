7df7fdeeb3ed2d7072b85efabed89cab7264715b
Brian Owen <briano.bend@icloud.com>
2019-08-02 06:30:34 -0700

    Adding more scores to the workout loader

--- scoring/deschutesDemoScores/views.py
+++ scoring/deschutesDemoScores/views.py
@@ -9,7 +9,7 @@ from .importing import DDDataImport
 def index(request):
     template = loader.get_template('scoring/index.html')
     #TODO: accept workout arguments
-    DDDataImport.importDDData()
+    #DDDataImport.importDDData()
     listOfScores = totals.getSingleWorkoutTotal(6)
 
     context = {'scores' : listOfScores}
