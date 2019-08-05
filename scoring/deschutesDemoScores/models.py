f7110d245b4f09a1083380eb343de4d7c229dac9
Brian Owen <briano.bend@icloud.com>
2019-08-05 13:44:36 -0700

    adding divisions to the model

--- scoring/deschutesDemoScores/models.py
+++ scoring/deschutesDemoScores/models.py
@@ -16,18 +16,27 @@ class Workout(models.Model):
 		max_length=1,
 		choices=scoringStyleChoices,
 		default='T')
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.description
 
 class Team(models.Model):
 	teamID = models.CharField(max_length=10, primary_key=True)
 	description = models.CharField(max_length=100)
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.description
+
+class Division(models.Model):
+	description = models.CharField(max_length=100)
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.description	
 
 class Athlete(models.Model):
 	description = models.CharField(max_length=100)
 	team = models.ForeignKey(Team, on_delete=models.CASCADE)
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.description
 
@@ -36,8 +45,10 @@ class Score(models.Model):
 	minutes = models.IntegerField(null=True, blank=True)
 	seconds = models.IntegerField(null=True, blank=True)
 	reps = models.IntegerField(null=True, blank=True)
+	division = models.ForeignKey(Division, on_delete=models.CASCADE)
 	team = models.ForeignKey(Team, on_delete=models.CASCADE)
 	workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
 	def __str__(self):
 		return str(self.team.description) + ' ' + str(self.workout.description)
     
