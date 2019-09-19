97bceb7de7ab30ceb93aa9de6ec6616ad4aaf189
Brian Owen <briano.bend@icloud.com>
2019-09-18 20:55:15 -0700

    Adding save logic to the post request when scores are updated

--- scoring/deschutesDemoScores/views.py
+++ scoring/deschutesDemoScores/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from .models import Team, Score, Workout, Division
+from .models import Team, Score, Workout, Division, Event
 from .totaling import totals
 from .importing import DDDataImport
 
@@ -69,7 +69,32 @@ def scoreInput(request):
 
 def scoreInputReceived(request):
     print(request.POST)
-    listOfTeams = request.POST['team']
+    try:
+        #print(request.POST['workout'])
+        #print(request.POST['division'])
+        workout = request.POST['workout']
+        division = request.POST['division']
+    except:
+        #print('no post available')
+        workout = 6
+        division = 1
+    listOfTeams = request.POST.getlist('team')
+    listOfMinutes = request.POST.getlist('minutes')
+    listOfSeconds = request.POST.getlist('seconds')
+    listOfWeight = request.POST.getlist('weight')
+    listOfReps = request.POST.getlist('reps')
+    for (i, teamScore) in enumerate(listOfTeams):
+        score = Score.objects.get(team=listOfTeams[i],workout=workout,event=1)
+        score.weight = listOfWeight[i]
+        score.reps = listOfReps[i]
+        score.minutes = listOfMinutes[i]
+        score.seconds = listOfSeconds[i]
+        score.save()
+        print(score)
+        
+        #except:
+        #    print('unable to write row to database ' + str(i))
+
     #for i in request.POST:
     #    print(i)
     template = loader.get_template('scoring/scoreInputReceived.html')
