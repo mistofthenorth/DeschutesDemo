7433973dafb96be42a9f85e545aebb839535993a
Brian Owen <briano.bend@icloud.com>
2019-09-23 19:59:34 -0700

    Updated audit code for saving scores

--- scoring/deschutesDemoScores/views.py
+++ scoring/deschutesDemoScores/views.py
@@ -8,27 +8,19 @@ from .importing import DDDataImport
 
 def index(request):
     try:
-        #print(request.POST['workout'])
-        #print(request.POST['division'])
         workout = request.POST['workout']
         division = request.POST['division']
     except:
-        #print('no post available')
         workout = 6
         division = 1
-    #print('workout number is ' + str(workout))
+        
     template = loader.get_template('scoring/index.html')
     #DDDataImport.importDDTeams()
     #DDDataImport.importDDData()
     listOfWorkouts = Workout.objects.filter(event = 1)
     listOfDivisions = Division.objects.filter(event = 1)
-    #print(listOfWorkouts[1].id)
     listOfScores = totals.getSingleWorkoutTotal(workout,division)
     scoringStyle = Workout.objects.get(pk=workout)
-    #print(scoringStyle.scoringStyle)
-    #
-    #allWorkouts = totals.getAllWorkoutsTotal(1)
-    #print(listOfWorkouts[0][0].rank)
     #
     context = {'scores' : listOfScores, 'workouts' : listOfWorkouts, 'divisions' : listOfDivisions, 'scoringStyle' : scoringStyle.scoringStyle}
     return HttpResponse(template.render(context, request))
@@ -36,67 +28,59 @@ def index(request):
 
 def finalResults(request):
     try:
-        #print(request.POST['workout'])
-        #print(request.POST['division'])
         division = request.POST['division']
     except:
-        #print('no post available')
         division = 1
+
     listOfDivisions = Division.objects.filter(event = 1)
     allWorkouts = totals.getAllWorkoutsTotal(division)
-    #print(allWorkouts[1])
-    #print(allWorkouts[0][0].rank)
     template = loader.get_template('scoring/finalResults.html')
     context = {'divisions' : listOfDivisions, 'allWorkouts' : allWorkouts[0], 'workoutScores' : allWorkouts[1]}
     return HttpResponse(template.render(context, request))
 
 def scoreInput(request):
     try:
-        #print(request.POST['workout'])
-        #print(request.POST['division'])
         workout = request.POST['workout']
         division = request.POST['division']
     except:
-        #print('no post available')
         workout = 6
         division = 1
-    setOfTeams = Team.objects.filter(division = division)
+
     listOfScores = totals.getSingleWorkoutTotal(workout,division)
     sortedListOfScores = sorted(listOfScores, key=lambda x: (x.score.team_id))
     template = loader.get_template('scoring/scoreInput.html')
-    context = {'setOfTeams' : setOfTeams, 'scores' : sortedListOfScores}
+    context = {'scores' : sortedListOfScores}
     return HttpResponse(template.render(context, request))
 
 def scoreInputReceived(request):
     print(request.POST)
     try:
-        #print(request.POST['workout'])
-        #print(request.POST['division'])
         workout = request.POST['workout']
         division = request.POST['division']
     except:
-        #print('no post available')
         workout = 6
         division = 1
+    #Split into lists
     listOfTeams = request.POST.getlist('team')
     listOfMinutes = request.POST.getlist('minutes')
     listOfSeconds = request.POST.getlist('seconds')
     listOfWeight = request.POST.getlist('weight')
     listOfReps = request.POST.getlist('reps')
+    listOfSaveResults = []
+
     for (i, teamScore) in enumerate(listOfTeams):
         score = Score.objects.get(team=listOfTeams[i],workout=workout,event=1)
         score.weight = listOfWeight[i]
         score.reps = listOfReps[i]
         score.minutes = listOfMinutes[i]
         score.seconds = listOfSeconds[i]
-        score.save()
-        print(score)
+        try:
+            score.save()
+            listOfSaveResults.append('Updated score for team ' + listOfTeams[i] + ' to ' + str(score.minutes) + ' minutes, ' + str(score.seconds) + ' seconds, ' + str(score.reps) + ' reps and ' +  str(score.weight) + ' weight')
+        except:
+            listOfSaveResults.append('unable to save score for team ' + listOfTeams[i])
         
-        #except:
-        #    print('unable to write row to database ' + str(i))
-
-    #for i in request.POST:
-    #    print(i)
+        
     template = loader.get_template('scoring/scoreInputReceived.html')
-    context = {}
+    context = {'listOfSaveResults' : listOfSaveResults}
     return HttpResponse(template.render(context, request))
