b7c686bf0d993263c46f695e36a0e0313d98b43f
Brian Owen <briano.bend@icloud.com>
2019-10-21 19:52:37 -0700

    Added extended descriptions to the workout model

--- scoring/deschutesDemoScores/models.py
+++ scoring/deschutesDemoScores/models.py
@@ -10,6 +10,7 @@ class Event(models.Model):
 
 class Workout(models.Model):
     description = models.CharField(max_length=100)
+    description_extended = models.TextField(null=True)
     scoringStyleChoices = (
         ('W', 'weight'),
         ('R', 'reps'),
